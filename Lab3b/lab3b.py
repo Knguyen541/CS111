1ced4c5e09bd83312cf5caea87fc9436387cc94c
Bao Khanh Nguyen <baok@lnxsrv09.seas.ucla.edu>
2020-08-28 22:56:57 -0700

    worked more on lab3b.py

--- Lab3b/lab3b.py
+++ Lab3b/lab3b.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import sys
-debug = 0;
 
 def main():
+    debug = 0
+    reserved_blocks = set([0,1])
     if len(sys.argv) <= 1:
         sys.stderr.write('Needs a file summary csv-file as argument\n')
         exit(1)
@@ -22,10 +23,29 @@ def main():
         output_type = fields[0]
         
         if output_type == 'SUPERBLOCK':
-            blocks_count = fields[1]
-            inode_count = fields[2]
+            blocks_count = int(fields[1])
+            inode_count = int(fields[2])
+            free_blocks = [1]*(blocks_count+1)
+            free_inodes = [1]*(inode_count+1)
+            if fields[3] == '1024':
+                reserved_blocks.add(2)
             if debug:
                 print("Blocks_count: {}, Inode_count: {}".format(blocks_count, inode_count))
+        elif output_type == 'GROUP':
+            reserved_blocks.add(int(fields[6]))
+            reserved_blocks.add(int(fields[7]))
+            reserved_blocks.add(int(fields[8]))
+            if debug:
+                print("Reserved blocks: {}".format(reserved_blocks))
+        elif output_type == 'BFREE':
+            free_blocks[int(fields[1])] = 0
+        elif output_type == 'IFREE':
+            free_inodes[int(fields[1])] = 0
+
+    if debug:
+        print("Free block array: {}".format(free_blocks))
+        print("Free inode array: {}".format(free_inodes))
+            
 
 if __name__ == "__main__":
     main()
